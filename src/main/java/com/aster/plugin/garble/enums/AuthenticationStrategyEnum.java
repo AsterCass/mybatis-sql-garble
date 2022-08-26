567cc7dbcce70e548812e254e9460744e5ce0125
astercasc <astercas@qq.com>
2022-08-26 16:21:41 +0800

    fix: enum 校正

--- src/main/java/com/aster/plugin/garble/enums/AuthenticationStrategyEnum.java
+++ src/main/java/com/aster/plugin/garble/enums/AuthenticationStrategyEnum.java
@@ -10,17 +10,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AuthenticationStrategyEnum {
 
+    /**
+     * 当传入鉴权code和数据库相应字段相等时
+     */
+    EQUAL(1, "相等"),
+
 
     /**
      * 当传入鉴权code和数据库相应字段按位&大于0时
      */
-    BOOLEAN_AND(1, "按位与"),
-
-
-    /**
-     * 当传入鉴权code和数据库相应字段相等时
-     */
-    EQUAL(2, "相等"),
+    BOOLEAN_AND(2, "按位与"),
 
 
 
